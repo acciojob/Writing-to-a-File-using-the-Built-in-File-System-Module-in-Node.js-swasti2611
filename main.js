37a16ba9a9813791bc96fe43e472d52f51e8ee09
Swati jagtap <swatijagtap5060@gmail.com>
2024-06-22 14:38:42 +0530

    submit

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 const fs = require('fs');
-
+const http=require('http')
 // TODO: Write the message "Hello, World!" to the file "output.txt"
 
+const server=http.createServer((res,req)=>{
+  fs.writeFile("./output.txt","Hello, World!",(err)=>{
+    if(err){
+        console.log(err);
+    }
+  })
+})
+server.listen(3000,()=>{
+    console.log("sever started");
+})
